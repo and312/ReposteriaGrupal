e01a02abce4c765bb2dee2df59fdbb6691b5a097
and312 <andreapelaez436@gmail.com>
2023-10-12 20:24:42 -0400

    solution error

---
+++ src/main/java/datosbd/ReposteriaGrupal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package datosbd;
+
+/**
+ *
+ * @author sound
+ */
+public class ReposteriaGrupal {
+
+    public static void main(String[] args) {
+        Conexion.conectarFirebase();
+    }
+}
