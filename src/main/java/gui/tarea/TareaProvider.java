3c6de78f2afa1c0f146fff514d605088e95d53bf
YAR-25 <tekken.6de@gmail.com>
2023-10-19 20:52:22 -0400

    Tarea Interfaz para docente y tareas terminado

--- src/main/java/gui/tarea/TareaProvider.java
+++ src/main/java/gui/tarea/TareaProvider.java
@@ -4,10 +4,18 @@ package gui.tarea;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.Precondition;
+import com.google.cloud.firestore.QuerySnapshot;
 import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.cloud.FirestoreClient;
+import datosbd.Conexion;
+import java.util.Collection;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 /**
  * @author Yawar-Nicohl
@@ -26,6 +34,50 @@ public class TareaProvider {
             DocumentReference docRef = db.collection(coleccion).document(documento);
             ApiFuture<WriteResult> result = docRef.set(data);
             System.out.println("Guardado correctamente");
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error: "+e.getMessage());
+        }
+        return false;
+    }
+    
+    public static void cargarTablaTarea(JTable table){
+        DefaultTableModel model = new DefaultTableModel();
+        model.addColumn("Id");
+        model.addColumn("Titulo");
+        model.addColumn("Instrucciones");
+        model.addColumn("Nota");
+        model.addColumn("Fecha");
+        
+        
+        try {
+            CollectionReference tareas = Conexion.db.collection("Tarea");
+            ApiFuture<QuerySnapshot> querySnap = tareas.get();
+            
+            for(DocumentSnapshot document: querySnap.get().getDocuments()){
+                model.addRow(new Object[]{
+                    document.getId(),
+                    document.getString("Titulo"),
+                    document.getString("Instrucciones"),
+                    document.getString("Nota"),
+                    document.getString("Fecha")
+                });
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            System.err.println("Error: "+e.getMessage());
+        }
+        
+        table.setModel(model);
+    }
+    
+    public static boolean eliminarTarea(String coleccion, String documento){
+        
+        db = FirestoreClient.getFirestore();
+        
+        try {
+            DocumentReference docRef = db.collection(coleccion).document(documento);
+            ApiFuture<WriteResult> result = docRef.delete(Precondition.NONE);
+            System.out.println("Eliminado correctamente");
             return true;
         } catch (Exception e) {
             System.out.println("Error: "+e.getMessage());
