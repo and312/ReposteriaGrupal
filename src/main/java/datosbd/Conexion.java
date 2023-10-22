907d23779355acb7b675916a70a7a808c9252b1f
YAR-25 <tekken.6de@gmail.com>
2023-10-22 05:42:43 -0400

    Error de la bd arreglado por YAR y NAE

--- src/main/java/datosbd/Conexion.java
+++ src/main/java/datosbd/Conexion.java
@@ -7,26 +7,33 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.List;
 
-/**
- *
- * @author sound
- */
+
 public class Conexion {
-    
+
     public static Firestore db;
-    
+
     public static void conectarFirebase() {
         try {
-            FileInputStream sa = new FileInputStream("postres.json");
+            // Verificar si ya existe una instancia de FirebaseApp.
+            List<FirebaseApp> firebaseApps = FirebaseApp.getApps();
+            if(firebaseApps!=null && !firebaseApps.isEmpty()){ // Si no está vacío, ya hay una instancia de Firebase App.
+                for(FirebaseApp app : firebaseApps){
+                    if(app.getName().equals(FirebaseApp.DEFAULT_APP_NAME))
+                        db = FirestoreClient.getFirestore(app);
+                }
+            } else { // Si está vacío, inicializamos una nueva instancia.
+                FileInputStream sa = new FileInputStream("postres.json");
+
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(sa))
+                        .build();
+
+                FirebaseApp.initializeApp(options); // Aquí ocurre la inicialización.
+                db = FirestoreClient.getFirestore();
+            }
             
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(sa))
-                    .build();
-            
-            FirebaseApp.initializeApp(options);
-            
-            db = FirestoreClient.getFirestore();
             System.out.println("Éxito al conectar");
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
